f380cd513da84902f39b844251fd925593c43bd2
WandersoNoleto <onlywanderson@gmail.com>
2022-03-24 20:58:07 -0300

    24.03 stage III

--- scripts/main.js
+++ scripts/main.js
@@ -11,38 +11,101 @@ const Modal = {
     }
 }
 
+//Alterar a cor do card total de acordo com o montante final :
+// Negativo - vermelho
+// Positivo - verde 
+const DisplayTotal = {
+    plus(){
+        document
+            .querySelector('.card.total')
+            .classList.add('plus')
+    },
+    minus(){
+        document
+        .querySelector('.card.total')
+        .classList.add('minus')
+    }
+}
 
-const transactions = [{
+
+const transactions = [
+    {
         id: 1, 
         description: 'Luz', 
         amount: -50000, 
-        date: '23/01/2021'
+        date: '23/01/2021',
     }, 
     {
         id: 2, 
         description: 'Frelancer', 
-        amount: 5000000, 
-        date: '21/01/2021'
+        amount: 500000, 
+        date: '21/01/2021',
     },
     {
         id: 3, 
         description: 'Internet', 
         amount: -20000,
-        date: '20/01/2021'
+        date: '20/01/2021',
     }]
 
 
 const Transaction = {
-    incomes(){ //somar entradas
+    all: transactions,
+    add(transaction){
+        transaction.all.push(transaction)
+    },
 
+    incomes(){ //somar entradas
+        let income = 0;
+        
+        transactions.all.forEach(transaction => {
+            if(transaction.amount > 0){
+                income += transaction.amount;
+            }
+        })
+
+        return income
     },
     expenses(){ //somar saídas
-
+        let expense = 0;
+        
+        transactions.all.forEach(transaction => {
+            if(transaction.amount < 0){
+                expense += transaction.amount;
+            }
+        })
+        
+        return expense
     },
     total(){
+        let total = 0;
 
+        total = Transaction.incomes() + Transaction.expenses();
+
+
+        return total
     }
 }
+
+//Formatar moeda para o padrão R$ (real)
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "- " : "+ "
+
+        //transformando montante em um string
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal+value
+    }
+}
+
 
 //Transferir dados das transações para o HTML
 const DOM = {
@@ -52,22 +115,48 @@ const DOM = {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHtmltransaction(transaction)
 
+        console.log(tr.innerHTML)
+
         DOM.transactionsContainer.appendChild(tr)
-        console.log(tr)
     },
 
-    innerHtmltransaction(){
-        const html = `
-            <td class="description">${transactions.description}</td>
-            <td class="expense">${transactions.amount}</td>
-            <td class="date">${transactions.date}</td>
-            <td><img src="/assets/minus.svg" alt="Remover transação"></td>`
+    innerHtmltransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
+         //Chamando formatação
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td><img src="/assets/minus.svg" alt="Remover transação"></td>
+            `
         return html
+    },
+
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+        
+        if(Transaction.total() > 0) {
+            DisplayTotal.plus();
+        } else if (Transaction.total() < 0){
+            DisplayTotal.minus();
+        }
     }
 }
 
+//Criando a tabela inicial
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
 
-DOM.addTransaction(transactions[0])
-DOM.addTransaction(transactions[1])
-DOM.addTransaction(transactions[2])
+DOM.updateBalance()
