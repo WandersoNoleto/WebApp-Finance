5825219f4553a1fa748ec65ab7a1684a92a88b76
WandersoNoleto <onlywanderson@gmail.com>
2022-04-03 17:32:00 -0300

    bpatch 1.3.4

--- scripts/main.js
+++ scripts/main.js
@@ -159,7 +159,7 @@ const DOM = {
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
-            <td><img onclick="Transaction.remove(${index})" src="/assets/minus.svg" alt="Remover transação"></td>
+            <td><span id="lineRemove" onclick="Transaction.remove(${index})">x</span></td>
             `
         return html
     },
